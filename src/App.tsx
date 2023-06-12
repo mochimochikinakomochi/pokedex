c8f57618c9bc97c53a16d954e6387ed111a51a29
HirokiShiba <hiroki3924hs@gmail.com>
2023-06-12 13:06:25 +0900

    a

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,19 @@ import { PokemonInfo } from './routes/PokemonInfo'
 import { PokemonList } from './routes/PokemonList'
 import { AiFillHome } from 'react-icons/ai'
 
-const homeUrl = process.env.PUBLIC_URL || '/'
 
 export const App = () => {
   return (
     <BrowserRouter>
       <div>
         <div>
-          <Link to={homeUrl} className="">
+          <Link to='/' className="">
             <AiFillHome size={50} color={'#FFF'}/>
           </Link>
         </div>
         <Routes>
-          <Route path={homeUrl} element={<Home />} />
-          <Route path={homeUrl + '/PokemonList'} element={<PokemonList />}>
+          <Route path='/' element={<Home />} />
+          <Route path='/PokemonList' element={<PokemonList />}>
             <Route path=':ID' element={<PokemonInfo />} />
           </Route>
         </Routes>
