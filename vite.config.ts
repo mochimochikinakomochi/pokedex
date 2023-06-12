118d5aa68b6dcb196ef84b28f93303e61e13e740
HirokiShiba <hiroki3924hs@gmail.com>
2023-06-12 21:59:23 +0900

    a

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",    
+  server: {
+    host: true,
+    port: 3000,
+  },
+  base: "/Home/",    
   plugins: [react()],   
 })
